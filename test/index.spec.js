3998a978cdde0482534c2bd0791ef7d327ba68f9
Ivan Uruskiy <iujohny12@gmail.com>
2020-01-10 17:29:44 -0800

    Initial commit

---
+++ test/index.spec.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+const {sum, mult} = require('../index.js');
+
+describe('function sum', () => {
+    it('should function sum equals 10', () => {
+        assert.equal(sum(2, 4), 6);
+    } );
+
+    it('should function sum not equals 0', () => {
+        assert.notEqual(sum(2, 4), 0);
+    });
+
+});
+
+it('should mult works correct 8', () => {
+    assert.equal(mult(2, 4 ), 8)
+});
